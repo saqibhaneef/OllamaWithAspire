28c9043f1ba0f0d84ee8b3ee05862d95ad79426d
saqibhaneef <muhammadsaqib.ms228@gmail.com>
2025-12-10 13:03:04 +0500

    added ollama

--- OllamaWithAspire.AppHost/AppHost.cs
+++ OllamaWithAspire.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var ollama = builder.AddOllama("ollama")
+               .WithDataVolume()
+               .WithOpenWebUI();
+
+ollama.AddModel("chat", "llama3.2");
+
 builder.Build().Run();
